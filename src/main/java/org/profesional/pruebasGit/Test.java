1d8fd0b2c64b70b7472d90b43a6a3674cc815256
Antonio Josué Rodriguez <josue.rodriguez@iteso.mx>
2022-06-24 11:54:54 -0500

    modificacion 2

--- src/main/java/org/profesional/pruebasGit/Test.java
+++ src/main/java/org/profesional/pruebasGit/Test.java
@@ -4,8 +4,11 @@ public class Test {
 
     public static void main(String[] args) {
         for (int i = 0; i <15 ; i+=2) {
-            System.out.println("foo");
-            System.out.println("holis")
+            if(i == 12){
+               System.out.println("foo");
+            }
+            else
+               System.out.println("holis")
         }
     }
 }
