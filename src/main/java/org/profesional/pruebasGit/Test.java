23253a0cc8e1266de7c81afa48b9c6e0160ccdca
Antonio Josué Rodriguez <josue.rodriguez@iteso.mx>
2022-06-24 11:46:35 -0500

    prueba 1

--- src/main/java/org/profesional/pruebasGit/Test.java
+++ src/main/java/org/profesional/pruebasGit/Test.java
@@ -3,8 +3,9 @@ package org.profesional.pruebasGit;
 public class Test {
 
     public static void main(String[] args) {
-        for (int i = 0; i <10 ; i++) {
+        for (int i = 0; i <15 ; i+=2) {
             System.out.println("foo");
+            System.out.println("holis")
         }
     }
 }
